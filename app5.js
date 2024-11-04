8539b35aad3786f7b4b7592621e5026e4ff35228
Ginji Saito <s24G1060RJ@s.chibakoudai.jp>
2024-11-04 23:40:35 +0900

    1105の授業のための修正

--- app5.js
+++ app5.js
@@ -31,17 +31,36 @@ app.get("/janken", (req, res) => {
   let hand = req.query.hand;
   let win = Number( req.query.win );
   let total = Number( req.query.total );
+  if( win!=NaN ) {
+    if (win>0);
+    else win=0;
+  }
+  if( total!=NaN ) {
+    if (total>0);
+    else total=0;
+  }
   console.log( {hand, win, total});
   const num = Math.floor( Math.random() * 3 + 1 );
   let cpu = '';
+  let my_num = 0;
+  let judgement='';
+
   if( num==1 ) cpu = 'グー';
   else if( num==2 ) cpu = 'チョキ';
   else cpu = 'パー';
   // ここに勝敗の判定を入れる
-  // 今はダミーで人間の勝ちにしておく
-  let judgement = '勝ち';
-  win += 1;
+  if( hand == 'グー') my_num = 4;
+  else if( hand == 'チョキ') my_num = 5;
+  else if( hand == 'パー') my_num = 6;
+  let num_diff=my_num-num;
+  if (num_diff%3==0) judgement='引き分け';
+  else if(num_diff%3==1) judgement='負け';
+  else {
+    judgement='勝ち';
+    win += 1;
+  }
   total += 1;
+
   const display = {
     your: hand,
     cpu: cpu,
